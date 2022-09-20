c2484eb1826a1ea994afe5938f0b96c4da60a8f4
Dominik Niemietz <d.niemietz97@gmx.de>
2022-09-20 21:58:12 +0200

    adding dialog-edit-address and dialog-edit-user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,10 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatCardModule} from '@angular/material/card';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { DialogEditAddressComponent } from './dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from './dialog-edit-user/dialog-edit-user.component';
+
 
 
 
@@ -34,7 +38,9 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     DashboardComponent,
     UserComponent,
     DialogAddUserComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    DialogEditAddressComponent,
+    DialogEditUserComponent
   ],
   imports: [
     BrowserModule,
@@ -54,7 +60,8 @@ import { UserDetailComponent } from './user-detail/user-detail.component';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     MatProgressBarModule,
-    MatCardModule
+    MatCardModule,
+    MatMenuModule
    
   ],
   providers: [],
--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute,
+      private firestore: AngularFirestore,
+      public dialog: MatDialog) { }
+
+  userId = '';
+  user: User = new User();
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap =>{
+      this.userId = paramMap.get('id');
+      console.log('GOT ID', this.userId);
+      this.getUser();
+    })
   }
 
+  getUser(){
+    this.firestore
+    .collection('users')
+    .doc(this.userId)
+    .valueChanges()
+    .subscribe((user: any) =>{
+      this.user = new User(user);
+      console.log('Retrieved user', this.user);
+    });
+  }
+
+  editMenu(){
+const dialog = this.dialog.open(DialogEditAddressComponent);
+    dialog.componentInstance.user = this.user;
+  }
+
+  editUserDetail(){
+    const dialog = this.dialog.open(DialogEditUserComponent);
+    dialog.componentInstance.user = this.user
+  }
 }
